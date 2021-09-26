b386640dba7b66f8a5f28285e4cfac2eedd16414
Suyooo <me@suyo.be>
2021-09-26 16:25:16 +0200

    Add script to test offline without Discord

---
+++ offline-tester.js
@@ -0,0 +1,28 @@
+const lookup = require("./commands/lookup.js");
+const log = require("./logger");
+const {loadDB} = require("./db");
+
+const stdin = process.openStdin();
+
+stdin.addListener("data", function(d) {
+    lookup.execute({
+        options: {
+            getString: () => d.toString().trim()        },
+        user: {
+            tag: "OfflineTester#0000"
+        },
+        reply: (reply) => {
+            if (reply.components && reply.components.length > 0) {
+                for (let i in reply.components[0].components[0].options) {
+                    log.debug("TEST", reply.components[0].components[0].options[i].label);
+                }
+            } else if (reply.content && reply.content !== "") log.debug("TEST", reply.content);
+            else log.debug("TEST", reply.embeds[0].title);
+        }
+    });
+});
+
+(async function() {
+    await loadDB();
+    console.log("You can now enter a lookup term.")
+})();
